10fe9dcd4a508e18a3676707fd347d23f3837ca6
Nand <nikjk2004@gmail.com>
2024-10-28 11:51:55 +0300

    Add new task 3

--- src/main/java/ru/mtuci/rbpo2/config/JwtTokenFilter.java
+++ src/main/java/ru/mtuci/rbpo2/config/JwtTokenFilter.java
@@ -1,11 +1,15 @@
-package ru.mtuci.rbpo2.config;
+package ru.mtuci.rbpo_2024_praktika.configuration;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -22,10 +26,25 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
         String token = resolveToken(request);
 
-        //TODO: написать тело метода для получения и проверки токена
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            String username = jwtTokenProvider.getUsername(token);
+
+            // Проверка отсутствия аутентификации в контексте безопасности
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                if (jwtTokenProvider.validateToken(token, userDetails.getUsername())) {
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    // Устанавливаем аутентификацию в контексте безопасности
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
 
         filterChain.doFilter(request, response);
-
     }
 
     private String resolveToken(HttpServletRequest request) {
